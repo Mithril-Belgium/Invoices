c87b766ad882537c63ba298d565b786733175851
Dominic Collart <dominic.collart@outlook.fr>
2019-01-27 22:01:28 +0100

    Implement ValueObject base class + tests

---
+++ src/Mithril.Invoices.Domain/Core/ValueObject.cs
@@ -0,0 +1,41 @@
+﻿namespace Mithril.Invoices.Domain.Core
+{
+    public abstract class ValueObject<T>
+        where T : ValueObject<T>
+    {
+        public override bool Equals(object obj)
+        {
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj is T other)
+            {
+                return EqualsCore(other);
+            }
+
+            return false;
+        }
+
+        public abstract bool EqualsCore(T obj);
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();
+        }
+
+        protected abstract int GetHashCodeCore();
+
+        public static bool operator ==(ValueObject<T> first, ValueObject<T> second)
+        {
+            return !ReferenceEquals(first, null) && first.Equals(second);
+        }
+
+        public static bool operator !=(ValueObject<T> first, ValueObject<T> second)
+        {
+            return !(first == second);
+        }
+    }
+}
---
+++ tests/Mithril.Invoices.Domain.Tests/Core/ValueObjectTests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using Mithril.Invoices.Domain.Core;
+using System;
+using Xunit;
+
+namespace Mithril.Invoices.Domain.Tests.Core
+{
+    public class ValueObjectTests
+    {
+        class FakeValueObject : ValueObject<FakeValueObject>
+        {
+            public int A { get; set; }
+            public string B { get; set; }
+
+            public override bool EqualsCore(FakeValueObject obj)
+            {
+                return A == obj.A && B == obj.B;
+            }
+
+            protected override int GetHashCodeCore()
+            {
+                return HashCode.Combine(A, B);
+            }
+        }
+
+        [Fact]
+        public void SimilarValueObjectsShouldEquals()
+        {
+            // Arrange
+            var valueObject1 = new FakeValueObject() { A = 5, B = "Hello" };
+            var valueObject2 = new FakeValueObject() { A = 5, B = "Hello" };
+
+            // Act
+            var areEquals = valueObject1 == valueObject2;
+
+            // Assert
+            areEquals.Should().BeTrue();
+        }
+
+        [Fact]
+        public void NotSimilarValueObjectsShouldNotEquals()
+        {
+            // Arrange
+            var valueObject1 = new FakeValueObject() { A = 5, B = "Hello" };
+            var valueObject2 = new FakeValueObject() { A = 6, B = "World" };
+
+            // Act
+            var areEquals = valueObject1 == valueObject2;
+
+            // Assert
+            areEquals.Should().BeFalse();
+        }
+
+        [Fact]
+        public void SimilarValueObjectsShouldHaveSameHashCode()
+        {
+            // Arrange
+            var valueObject1 = new FakeValueObject() { A = 5, B = "Hello" };
+            var valueObject2 = new FakeValueObject() { A = 5, B = "Hello" };
+
+            // Act
+            var sameHashCode = valueObject1.GetHashCode() == valueObject2.GetHashCode();
+
+            // Assert
+            sameHashCode.Should().BeTrue();
+        }
+
+        [Fact]
+        public void SimilarValueObjectsShouldNotHaveSameHashCode()
+        {
+            // Arrange
+            var valueObject1 = new FakeValueObject() { A = 5, B = "Hello" };
+            var valueObject2 = new FakeValueObject() { A = 6, B = "World" };
+
+            // Act
+            var sameHashCode = valueObject1.GetHashCode() == valueObject2.GetHashCode();
+
+            // Assert
+            sameHashCode.Should().BeFalse();
+        }
+
+    }
+}
