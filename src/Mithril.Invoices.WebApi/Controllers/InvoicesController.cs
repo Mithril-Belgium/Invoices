fa71c957d1b5cbd493ee215d0b971dc8905e2811
Dominic Collart <dominic.collart@outlook.fr>
2019-02-06 22:13:01 +0100

    chore: add basic empty projects (application/web)

---
+++ src/Mithril.Invoices.WebApi/Controllers/InvoicesController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mithril.Invoices.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+    }
+}
