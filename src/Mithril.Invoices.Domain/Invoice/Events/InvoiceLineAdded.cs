330d6064bab3f736c58585f374075cedbac37faf
Dominic Collart <dominic.collart@outlook.fr>
2019-02-25 11:47:47 +0100

    feat: add invoice line (domain)

---
+++ src/Mithril.Invoices.Domain/Invoice/Events/InvoiceLineAdded.cs
@@ -0,0 +1,17 @@
+﻿using Mithril.Invoices.Domain.Core;
+
+namespace Mithril.Invoices.Domain.Invoice.Events
+{
+    public class InvoiceLineAdded : IDomainEvent
+    {
+        public int LineNumber { get; private set; }
+        public string Label { get; private set; }
+
+
+        public InvoiceLineAdded(int lineNumber, string label)
+        {
+            LineNumber = lineNumber;
+            Label = label;
+        }
+    }
+}
---
+++ src/Mithril.Invoices.Domain/Invoice/InvoiceLine.cs
@@ -0,0 +1,32 @@
+﻿using Mithril.Invoices.Domain.Core;
+using System;
+
+namespace Mithril.Invoices.Domain.Invoice
+{
+    public class InvoiceLine : ValueObject<InvoiceLine>
+    {
+        public int LineNumber { get; set; }
+
+        public string Label { get; }
+
+        public InvoiceLine(int lineNumber, string label)
+        {
+            LineNumber = lineNumber;
+            Label = label;
+        }
+
+        private InvoiceLine()
+        {
+        }
+
+        public override bool EqualsCore(InvoiceLine obj)
+        {
+            return LineNumber == obj.LineNumber && Label == obj.Label;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return HashCode.Combine(LineNumber, Label);
+        }
+    }
+}
